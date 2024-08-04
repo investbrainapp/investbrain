8ef8a2c9e94c1904ecf5a55e566fb908e70350f8
hackerESQ <corey@coreyvarma.com>
2024-08-04 18:27:15 -0500

    fix daisy ui conflicts with tailwind forms

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,10 @@
 import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
 import typography from '@tailwindcss/typography';
 
 /** @type {import('tailwindcss').Config} */
 export default {
 	daisyui: {
-		themes: ["business", "corporate"
-			// {
-			// 	dark: {	
-			// 		"primary": "#60a5fa",	
-			// 		"primary-content": "#030a15",	
-			// 		"secondary": "#1e3a8a",	
-			// 		"secondary-content": "#cdd6e9",	
-			// 		"accent": "#c084fc",	
-			// 		"accent-content": "#0e0616",	
-			// 		"neutral": "#111827",	
-			// 		"neutral-content": "#c9cbcf",	
-			// 		"base-100": "#1c2e38",	
-			// 		"base-200": "#17272f",	
-			// 		"base-300": "#121f27",	
-			// 		"base-content": "#cdd1d4",	
-			// 		"info": "#0284c7",	
-			// 		"info-content": "#00060e",	
-			// 		"success": "#00a900",	
-			// 		"success-content": "#000a00",	
-			// 		"warning": "#facc15",	
-			// 		"warning-content": "#150f00",	
-			// 		"error": "#b91c1c",	
-			// 		"error-content": "#f7d5d1",	
-			// 	},  
-			// },
-		]
+		themes: ["business", "corporate"]
 	},
     content: [
 		'./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
@@ -48,7 +22,6 @@ export default {
     },
 	darkMode: ['class', '[data-theme="business"]'],
     plugins: [
-		forms,
 		typography,
 		require("daisyui")
 	],
