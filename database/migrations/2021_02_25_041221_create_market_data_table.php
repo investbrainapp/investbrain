0b8dfd8ebf6b1b135b491b25ca47168c6e0f6fcb
hackerESQ <corey@coreyvarma.com>
2024-08-23 18:44:48 -0500

    allow placeholder symbol

--- database/migrations/2021_02_25_041221_create_market_data_table.php
+++ database/migrations/2021_02_25_041221_create_market_data_table.php
@@ -15,10 +15,10 @@ class CreateMarketDataTable extends Migration
     {
         Schema::create('market_data', function (Blueprint $table) {
             $table->string('symbol', 15)->primary();
-            $table->string('name');
-            $table->float('market_value', 12, 4);
-            $table->float('fifty_two_week_low', 12, 4);
-            $table->float('fifty_two_week_high', 12, 4);
+            $table->string('name')->nullable();
+            $table->float('market_value', 12, 4)->nullable();
+            $table->float('fifty_two_week_low', 12, 4)->nullable();
+            $table->float('fifty_two_week_high', 12, 4)->nullable();
             $table->timestamp('last_dividend_date')->nullable();
             $table->float('last_dividend_amount', 12, 4)->nullable();
             $table->timestamps();
