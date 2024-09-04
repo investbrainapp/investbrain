11e95e7b5f97a07c7fff8acc812a60b69ab1ba99
hackerESQ <corey@coreyvarma.com>
2024-09-04 18:37:41 -0500

    make sure we generate app key

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,10 +2,16 @@
 
 echo "Running entrypoint script..."
 
-if [ ! -f /var/www/app/.env ]; then
+if [ ! -f "/var/www/app/.env" ]; then
     echo "Ope, gotta create an .env file!"
 
     cp /var/www/app/.env.example /var/www/app/.env
+fi
+
+if ( ! grep -q "^APP_KEY=" "/var/www/app/.env" || grep -q "^APP_KEY=$" "/var/www/app/.env"); then
+    echo "Ah, APP_KEY is missing in .env file. Generating a new key!"
+    
+    /usr/local/bin/php /var/www/app/artisan key:generate
 fi
 
 echo "Installing Composer dependencies..."
