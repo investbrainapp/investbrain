95792c2fd427702f79663d699c0d3ef4c6d936ba
hackerESQ <corey@coreyvarma.com>
2024-09-05 18:32:29 -0500

    format titles better

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "===============   Running entrypoint script...    =============== "
+echo "====================== Running entrypoint script...  ====================== "
 
 if [ ! -f "/var/www/app/.env" ]; then
     echo " > Ope, gotta create an .env file!"
@@ -14,15 +14,15 @@ if ( ! grep -q "^APP_KEY=" "/var/www/app/.env" || grep -q "^APP_KEY=$" "/var/www
     /usr/local/bin/php /var/www/app/artisan key:generate
 fi
 
-echo "===============   Installing Composer dependencies...    =============== "
+echo "====================== Installing Composer dependencies...  ====================== "
 /usr/local/bin/composer install
 
-echo "===============   Install NPM dependencies and build frontend...    =============== "
+echo "====================== Install NPM dependencies and build frontend...  ====================== "
 /usr/bin/npm install 
 /usr/bin/npm run build 
 
-echo "===============   Running migrations...    =============== "
+echo "====================== Running migrations...  ====================== "
 /usr/local/bin/php /var/www/app/artisan migrate --force
 
-echo "===============   Spinning up Supervisor daemon...    =============== "
+echo "====================== Spinning up Supervisor daemon...  ====================== "
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
