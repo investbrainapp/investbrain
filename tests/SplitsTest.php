0c27ebaba576a482c48e463bf0dc31b2d1d787ce
hackerESQ <corey@coreyvarma.com>
2024-09-09 17:56:57 -0500

    test skeleton

---
+++ tests/SplitsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class SplitsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     */
+    public function test_user_has_dashboard_metrics(): void
+    {
+        //
+    }
+}
