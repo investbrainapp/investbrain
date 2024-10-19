6e7571358989d65600262c2600f7a73d7ba967d0
hackerESQ <corey@coreyvarma.com>
2024-10-18 22:21:12 -0500

    adds reinvested dividend to import

--- app/Imports/Sheets/TransactionsSheet.php
+++ app/Imports/Sheets/TransactionsSheet.php
@@ -35,6 +35,7 @@ class TransactionsSheet implements ToCollection, WithHeadingRow, WithValidation,
                                 'cost_basis' => $transaction['cost_basis'] ?? 0,
                                 'sale_price' => $transaction['sale_price'],
                                 'split' => $transaction['split'] ?? null,
+                                'reinvested_dividend' => $transaction['reinvested_dividend'] ?? null,
                                 'date' => $transaction['date'],
                             ]);
 
@@ -58,6 +59,8 @@ class TransactionsSheet implements ToCollection, WithHeadingRow, WithValidation,
             'transaction_type' => ['required', 'in:BUY,SELL'],
             'date' => ['required', 'date'],
             'quantity' => ['required', 'min:0', 'numeric'],
+            'split' => ['sometimes', 'nullable', 'boolean'],
+            'reinvested_dividend' => ['sometimes', 'nullable', 'boolean'],
             'cost_basis' => ['sometimes', 'nullable', 'min:0', 'numeric'],
             'sale_price' => ['sometimes', 'nullable', 'min:0', 'numeric'],
         ];
