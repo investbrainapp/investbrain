e97e927ca379bca047a14180d221ddb1d1886511
hackerESQ <corey@coreyvarma.com>
2024-10-20 13:15:34 -0500

    fix:mobile improvements

--- resources/views/components/section-border.blade.php
+++ resources/views/components/section-border.blade.php
@@ -1,7 +1,5 @@
 
-<div {{ $attributes->class(['h-4 sm:h-auto' => $attributes->has('hide-on-mobile')]) }}>
-    <div {{ $attributes->class(['py-6' => !$attributes->has('class')]) }}>
+<div {{ $attributes->class(['py-6' => !$attributes->has('class'), 'h-4 sm:h-auto' => $attributes->has('hide-on-mobile')]) }}>
 
-        <hr class="hidden sm:block border-t border-gray-200 dark:border-gray-700" />
-    </div>
+    <hr class="{{ $attributes->has('hide-on-mobile') ? 'hidden sm:block' : '' }} border-t border-gray-200 dark:border-gray-700" />
 </div>
