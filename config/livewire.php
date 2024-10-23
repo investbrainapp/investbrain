b441fb3953030d14961d3fedefbc283d98af63d6
hackerESQ <corey@coreyvarma.com>
2024-10-23 14:13:43 -0500

    fix:allow customizing livewire temp upload disk via env

--- config/livewire.php
+++ config/livewire.php
@@ -64,7 +64,7 @@ return [
     */
 
     'temporary_file_upload' => [
-        'disk' => null,        // Example: 'local', 's3'              | Default: 'default'
+        'disk' => env('TEMP_UPLOAD_DISK', null),        // Example: 'local', 's3'              | Default: 'default'
         'rules' => null,       // Example: ['file', 'mimes:png,jpg']  | Default: ['required', 'file', 'max:12288'] (12MB)
         'directory' => null,   // Example: 'tmp'                      | Default: 'livewire-tmp'
         'middleware' => null,  // Example: 'throttle:5,1'             | Default: 'throttle:60,1'
