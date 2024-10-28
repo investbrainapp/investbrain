41377757ec63bd6aff25522924f82477e2ebc2ef
hackerESQ <corey@coreyvarma.com>
2024-10-28 17:50:45 -0500

    fix:only sync daily changes for weekdays

--- app/Models/Portfolio.php
+++ app/Models/Portfolio.php
@@ -138,7 +138,12 @@ class Portfolio extends Model
 
         $holdings->each(function($holding) use (&$total_performance, $dividends) {
 
-            $period = CarbonPeriod::create($holding->first_transaction_date, now()->isBefore(Carbon::parse(config('investbrain.daily_change_time_of_day'))) ? now()->subDay() : now())->filter('isWeekday');
+            $period = CarbonPeriod::create(
+                $holding->first_transaction_date, 
+                now()->isBefore(Carbon::parse(config('investbrain.daily_change_time_of_day'))) 
+                    ? now()->subDay() 
+                    : now()
+            );
 
             $holding->setRelation('dividends', $dividends->where('symbol', $holding->symbol));
 
