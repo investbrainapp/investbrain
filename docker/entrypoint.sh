575fecb16344b7e7efdf34aa9a5d4b8e6339b90d
hackerESQ <corey@coreyvarma.com>
2025-01-25 21:43:58 -0600

    wip

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,34 +2,38 @@
 
 cd /var/www/app
 
-echo -e "\n====================== Running entrypoint script...  ====================== "
-if [ ! -f ".env" ]; then
-    echo " > Ope, gotta create an .env file!"
+su - www-data -c "
 
-    [ -d ".env" ] && rm -rf .env
+echo -e '\n====================== Running entrypoint script...  ====================== '
+
+
+if [ ! -f '.env' ]; then
+    echo ' > Ope, gotta create an .env file!'
+
+    [ -d '.env' ] && rm -rf .env
 
     cp .env.example .env
 fi
 
-echo -e "\n====================== Validating environment...  ====================== "
-if [ $(stat -c '%U' .) != "www-data" ]; then
-    echo " > Setting correct permissions for application..."
+echo -e '\n====================== Validating environment...  ====================== '
+if [ $(stat -c '%U' .) != 'www-data' ]; then
+    echo ' > Setting correct permissions for application...'
     chown -R www-data:www-data .
 fi
 
-if ( ! grep -q "^APP_KEY=" ".env" || grep -q "^APP_KEY=$" ".env"); then
-    echo " > Ah, APP_KEY is missing in .env file. Generating a new key!"
+if ( ! grep -q '^APP_KEY=' '.env' || grep -q '^APP_KEY=$' '.env'); then
+    echo ' > Ah, APP_KEY is missing in .env file. Generating a new key!'
     
     /usr/local/bin/php artisan key:generate --force
 fi
 
-if [ ! -L "public/storage" ]; then
-    echo " > Creating symbolic link for app public storage..."
+if [ ! -L 'public/storage' ]; then
+    echo ' > Creating symbolic link for app public storage...'
     
     /usr/local/bin/php artisan storage:link
 fi
 
-echo -e "\n====================== Running migrations...  ====================== "
+echo -e '\n====================== Running migrations...  ====================== '
 run_migrations() {
     /usr/local/bin/php artisan migrate --force
 }
@@ -38,12 +42,12 @@ DELAY=5
 until run_migrations; do
   RETRIES=$((RETRIES-1))
   if [ $RETRIES -le 0 ]; then
-    echo " > Database is not ready after multiple attempts. Exiting..."
+    echo ' > Database is not ready after multiple attempts. Exiting...'
     exit 1
   fi
-  echo " > Waiting for database to be ready... retrying in $DELAY seconds."
+  echo ' > Waiting for database to be ready... retrying in $DELAY seconds.'
   sleep $DELAY
 done
 
-echo -e "\n====================== Spinning up Supervisor daemon...  ====================== "
-exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+echo -e '\n====================== Spinning up Supervisor daemon...  ====================== '
+exec /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf"
