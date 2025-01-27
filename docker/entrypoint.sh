99749bd9c94bd7906a635d7c4977996d7b8516ad
hackerESQ <corey@coreyvarma.com>
2025-01-26 21:42:39 -0600

    this is it

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,18 +4,18 @@ cd /var/www/app
 
 echo -e "\n====================== Validating environment...  ====================== "
 if [[ -z "$APP_KEY" ]]; then
-    echo "\n > Oops! The required APP_KEY configuration is missing in your environment!  \n\n"
-    echo "\n > Generating a key (see below) but this will NOT be persisted between container restarts.  \n\n"
-    echo "\n > You should set this APP_KEY in your .env file!  \n\n"
+    echo -e "\n > Oops! The required APP_KEY configuration is missing in your environment! "
+    echo -e "\n > Generating a key (see below) but this will NOT be persisted between container restarts. "
+    echo -e "\n > You should set this APP_KEY in your .env file!  \n\n"
 
     draw_box() {
       local text="$1"
       local length=${#text}
       local border=$(printf '%*s' "$((length + 4))" | tr ' ' '*')
 
-      echo "$border"
+      echo -e "\n\n$border"
       echo "* $text *"
-      echo "$border"
+      echo -e "$border\n\n"
     }
 
     export APP_KEY=base64:$(openssl rand -base64 32)
@@ -24,7 +24,7 @@ fi
 
 for dir in storage/framework/cache storage/framework/sessions storage/framework/views; do
     if [ ! -d "$dir" ]; then
-        echo "\n > $dir is missing. Creating scaffold for storage directory... \n\n"
+        echo -e "\n > $dir is missing. Creating scaffold for storage directory... \n\n"
         mkdir -p storage/framework/{cache,sessions,views}
         chmod -R 775 storage
         chown -R www-data:www-data storage
@@ -32,7 +32,7 @@ for dir in storage/framework/cache storage/framework/sessions storage/framework/
 done
 
 if [ ! -L "public/storage" ]; then
-    echo "\n > Creating symbolic link for app public storage... \n\n"
+    echo -e "\n > Creating symbolic link for app public storage... \n\n"
     
     /usr/local/bin/php /var/www/app/artisan storage:link
 fi
@@ -46,10 +46,10 @@ DELAY=5
 until run_migrations; do
   RETRIES=$((RETRIES-1))
   if [ $RETRIES -le 0 ]; then
-    echo "\n > Database is not ready after $RETRIES attempts. Exiting... \n\n"
+    echo -e "\n > Database is not ready after $RETRIES attempts. Exiting... \n\n"
     exit 1
   fi
-  echo "\n > Waiting for database to be ready... retrying in $DELAY seconds. \n\n"
+  echo -e "\n > Waiting for database to be ready... retrying in $DELAY seconds. \n\n"
   sleep $DELAY
 done
 
