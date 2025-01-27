778d7991136819d8e1cb0451a916fb56c92af98e
hackerESQ <corey@coreyvarma.com>
2025-01-26 21:56:41 -0600

    cleanup

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,14 +6,14 @@ echo -e "\n====================== Validating environment...  ===================
 if [[ -z "$APP_KEY" ]]; then
     echo -e "\n > Oops! The required APP_KEY configuration is missing in your environment! "
     echo -e "\n > Generating a key (see below) but this will NOT be persisted between container restarts. "
-    echo -e "\n > You should set this APP_KEY in your .env file!  \n\n"
+    echo -e "\n > You should set this APP_KEY in your .env file! "
 
     draw_box() {
       local text="$1"
       local length=${#text}
       local border=$(printf '%*s' "$((length + 4))" | tr ' ' '*')
 
-      echo "$border"
+      echo -e "\n\n$border"
       echo "* $text *"
       echo "$border"
     }
@@ -24,7 +24,7 @@ fi
 
 for dir in storage/framework/cache storage/framework/sessions storage/framework/views; do
     if [ ! -d "$dir" ]; then
-        echo -e "\n > $dir is missing. Creating scaffold for storage directory... \n\n"
+        echo -e "\n > $dir is missing. Creating scaffold for storage directory... "
         mkdir -p storage/framework/{cache,sessions,views}
         chmod -R 775 storage
         chown -R www-data:www-data storage
@@ -32,7 +32,7 @@ for dir in storage/framework/cache storage/framework/sessions storage/framework/
 done
 
 if [ ! -L "public/storage" ]; then
-    echo -e "\n > Creating symbolic link for app public storage... \n\n"
+    echo -e "\n > Creating symbolic link for app public storage... "
     
     /usr/local/bin/php /var/www/app/artisan storage:link
 fi
@@ -46,10 +46,10 @@ DELAY=5
 until run_migrations; do
   RETRIES=$((RETRIES-1))
   if [ $RETRIES -le 0 ]; then
-    echo -e "\n > Database is not ready after $RETRIES attempts. Exiting... \n\n"
+    echo -e "\n > Database is not ready after $RETRIES attempts. Exiting... "
     exit 1
   fi
-  echo -e "\n > Waiting for database to be ready... retrying in $DELAY seconds. \n\n"
+  echo -e "\n > Waiting for database to be ready... retrying in $DELAY seconds. "
   sleep $DELAY
 done
 
