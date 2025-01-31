2ced6557fef220ec0e4de0a502294d1141eee8d9
hackerESQ <corey@coreyvarma.com>
2025-01-30 22:20:33 -0600

    feat: default curr USD for new users

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -38,6 +38,7 @@ class CreateNewUser implements CreatesNewUsers
             'name' => $input['name'],
             'email' => $input['email'],
             'password' => Hash::make($input['password']),
+            'currency' => 'USD',
         ]);
     }
 }
