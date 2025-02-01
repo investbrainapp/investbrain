bd8017e44395c97e637914a9a789040ffadc6858
hackerESQ <corey@coreyvarma.com>
2025-02-01 14:11:39 -0600

    test: add test for seeding market data

--- tests/MarketDataTest.php
+++ tests/MarketDataTest.php
@@ -5,11 +5,23 @@ declare(strict_types=1);
 namespace Tests;
 
 use App\Models\MarketData;
+use Database\Seeders\MarketDataSeeder;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Facades\Artisan;
 
 class MarketDataTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function test_can_seed_market_data()
+    {
+        Artisan::call('db:seed', [
+            '--class' => MarketDataSeeder::class,
+            '--force' => true,
+        ]);
+
+        $this->assertEquals(14464, MarketData::count('symbol'));
+    }
 
     public function test_can_get_quote_from_provider()
     {
