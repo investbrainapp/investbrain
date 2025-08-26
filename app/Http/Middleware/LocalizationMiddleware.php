750ccbd68fe1a69b62fb1fd9e323f85d37007a62
hackerESQ <corey@coreyvarma.com>
2025-08-25 19:58:53 -0500

    fix: locale setting

--- app/Http/Middleware/LocalizationMiddleware.php
+++ app/Http/Middleware/LocalizationMiddleware.php
@@ -5,11 +5,10 @@ declare(strict_types=1);
 namespace App\Http\Middleware;
 
 use Closure;
-use Illuminate\Support\Str;
 use Illuminate\Http\Request;
-use Illuminate\Support\Number;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Foundation\Events\LocaleUpdated;
+use Illuminate\Support\Number;
+use Illuminate\Support\Str;
 
 class LocalizationMiddleware
 {
@@ -24,14 +23,12 @@ class LocalizationMiddleware
 
             $locale = auth()->user()->getLocale();
 
-            config(['app.locale' => $locale]);
-            app('translator')->setLocale(Str::before($locale, '_'));
-            app('events')->dispatch(new LocaleUpdated($locale));
+            app()->setLocale(Str::before($locale, '_'));
 
             Number::useLocale($locale);
             Number::useCurrency(auth()->user()->getCurrency());
         }
-        
+
         return $next($request);
     }
 }
